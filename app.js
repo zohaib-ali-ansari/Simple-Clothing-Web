c9c15b3b4c4935ebe20ac7020fc33a0658cfceb2
zohaib-ali-ansari <zohaibaliansari70@gmail.com>
2024-08-22 04:52:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let currentSlide = 0;
+let slides = document.querySelectorAll('.slide');
+
+function showSlide(n) {
+  slides[currentSlide].style.display = 'none';
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].style.display = 'block';
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+showSlide(currentSlide);
+setInterval(nextSlide, 2000);
